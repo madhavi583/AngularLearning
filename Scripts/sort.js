b1d8da3b2641222245144945d1cc0fdf81bc0304
madhavipathapati <madhavi.pathapati@olivetech.net>
2017-07-21 17:46:01 +0530

    madhavi sorting

--- Scripts/sort.js
+++ Scripts/sort.js
@@ -1,18 +1,73 @@
-ar myApp=angular.module('myModule',[]);
+var myApp=angular.module('myModule',[])
+//this create custom filter
+.filter("gender",function (){
+return function(gender){
+switch(gender){
+case 1:
+return "Male";
+case 2:
+return "FeMale";
+case 3:
+return "Not di";
+}
+}
+})
 myApp.controller('myController',function($scope){
 
 //
 var technologies= [
-    {firstName : "venkat", name: "PHP",likes:"0",dislike:"0",dateOfBirth:new Date('november 23,1980 '),gender : "male",salary:"100000"},
+ {firstName : "venkat", name: "PHP",likes:"0",dislike:"0",dateOfBirth:new Date('november 23,1980 '),gender : "male",salary:"100000"},
  {firstName : "venkat",name: "DRUPAL",likes:"0",dislike:"0", dateOfBirth:new Date('may 05 23,1970 '), gender : "Female",salary:"2000"},
  {firstName : "Hannah",name: "MYSQL",likes:"0",dislike:"0",dateOfBirth:new Date('August 15,1960 '),gender : "Female",salary:"1000"},
   { firstName : "Madhavi",name: "ANGULAR",likes:"0",dislike:"0",dateOfBirth:new Date('August 15,1960 '),gender : "Female",salary:"1000"},
  ];
  $scope. technologies= technologies;
- var employee ={
-        firstName : "venkat",
-        lastNmae : "pathapati",
-        gender : "male"
-    };
-    $scope. employee= employee;
+ $scope.sortCOlumn="name";
+ $scope.reverseSort =false;
+ $scope.sortData= function(column){
+ $scope.reverseSort=($scope.sortCOlumn == column)? !$scope.reverseSort :false;
+$scope.sortCOlumn = column;
+ }
+ $scope.getSortClass =function(column){
+if($scope.sortCOlumn == column){
+    return $scope.reverseSort ? 'arrow-down':'arrow-up'
+
+}
+return '';
+ }
+
+//  var employee ={
+//         firstName : "venkat",
+//         lastNmae : "pathapati",
+//         gender : "male"
+//     };
+    // $scope. employee= employee;
+   var employee=  [
+ {firstName : "venkat", name: "PHP",likes:"0",dislike:"0",dateOfBirth:new Date('november 23,1980 '),gender : "1",salary:"100000"},
+ {firstName : "venkat",name: "DRUPAL",likes:"0",dislike:"0", dateOfBirth:new Date('may 05 23,1970 '), gender : "2",salary:"2000"},
+ {firstName : "Hannah",name: "MYSQL",likes:"0",dislike:"0",dateOfBirth:new Date('August 15,1960 '),gender : "Female",salary:"1000"},
+  { firstName : "Madhavi",name: "ANGULAR",likes:"0",dislike:"0",dateOfBirth:new Date('August 15,1960 '),gender : "Female",salary:"1000"}
+ ];
+ $scope. employee= employee;
+ $scope.search=function(item){
+if($scope.searchText == undefined)
+{
+return true;
+}
+else{
+    if(item.firstName.toLowerCase().indexOf($scope.searchText.toLowerCase()) !=-1 || item.gender.toLowerCase().indexOf($scope.searchText.toLowerCase()) !=-1)
+    {
+        return true;
+    }
+    return false;
+}
+ }
+  var customfilter=[{ 
+      name : "Ben",dateOfBirth:new Date('november 23,1980 '),gender : "male -1",salary:"100000"},
+ {name : "sara", dateOfBirth:new Date('may 05 23,1970 '), gender : "Female -2",salary:"2000"},
+ {name : "mark",dateOfBirth:new Date('August 15,1960 '),gender : "Female-1",salary:"1000"},
+  { name : "pam",dateOfBirth:new Date('August 15,1960 '),gender : "Female-2 ",salary:"1000"},
+  { name : "todd",dateOfBirth:new Date('August 15,1960 '),gender : "Female-3 ",salary:"1000"}
+ ];
+ $scope.customfilter=customfilter;
 });
